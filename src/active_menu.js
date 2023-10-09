300729bce5a13885c27f8f8171737491601f4ce7
BSL <90385793+8oi4rom5eoul@users.noreply.github.com>
2023-10-09 17:25:40 +0900

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -14,13 +14,18 @@ const sectionIds = [
   "#testimonial",
   "#contact",
 ];
+
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector('[href="${id}"]')
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(callback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -38,9 +43,18 @@ function observerCallback(entries) {
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
